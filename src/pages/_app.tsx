f120e67eb8676e8a370443ccd45c1aad413cb217
LapsTimeOFF <39061148+LapsTimeOFF@users.noreply.github.com>
2023-07-05 22:14:43 +0200

    Push code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { IntlProvider } from "react-intl";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  const { locale } = router;
+  return (
+    <IntlProvider locale={locale ?? "en"}>
+      <Component {...pageProps} />
+    </IntlProvider>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,28 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { useRouter } from "next/router";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { Html, Head, Main, NextScript } from "next/document";
+import { IntlProvider } from "react-intl";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <Main />
-        <NextScript />
+        <ThemeProvider
+          theme={createTheme({
+            palette: {
+              mode: "dark",
+              primary: {
+                main: "#ff0000",
+              },
+            },
+          })}
+        >
+          <CssBaseline />
+          <Main />
+          <NextScript />
+        </ThemeProvider>
       </body>
     </Html>
-  )
+  );
 }
