8a19ad45854740dbf993f3aadc91d9a8bf56078f
LapsTimeOFF <39061148+LapsTimeOFF@users.noreply.github.com>
2023-07-06 17:15:44 +0200

    Add twemoji emojis for better flags on windows

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,16 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import { IntlProvider } from "react-intl";
+import Twemoji from "react-twemoji";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const { locale } = router;
   return (
     <IntlProvider locale={locale ?? "en"}>
-      <Component {...pageProps} />
+      <Twemoji options={{ className: "twemoji" }}>
+        <Component {...pageProps} />
+      </Twemoji>
     </IntlProvider>
   );
 }
